59a6ffd2b1c433b18eee47e67a44359a1cb704ad
Julius <33764638+P3ntest@users.noreply.github.com>
2021-12-14 13:56:35 +0100

    add login

--- src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
+++ src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
@@ -3,6 +3,7 @@ package fr.christophetd.log4shell.vulnerableapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.apache.logging.log4j.LogManager;
@@ -14,9 +15,14 @@ public class MainController {
     private static final Logger logger = LogManager.getLogger("HelloWorld");
 
     @GetMapping("/")
-    public String index(@RequestHeader("X-Api-Version") String apiVersion) {
-        logger.info("Received a request for API version " + apiVersion);
-        return "Hello, world!";
+    public String index() {
+        return "<h1>Login zu meinem coolen Java Server</h1><form action=\"/login\" method=\"get\"><input type=\"text\" name=\"username\" id=\"username\" placeholder=\"username\"><button type=\"submit\">Login</button></form>";
+    }
+
+    @GetMapping("/login")
+    public String index(@RequestParam("username") String username) {
+        logger.info("Received a request to login with username: " + username);
+        return "Benutzer " + username + " nicht gefunden!";
     }
 
 }
